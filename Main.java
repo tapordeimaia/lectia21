d9ee5a7fec74fbe3d912bded98c8f2c6a4922a78
tapordeimaia <146013799+tapordeimaia@users.noreply.github.com>
2023-11-10 21:05:10 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<Integer> treeSet = new TreeSet<>();
+        treeSet.add(5);
+        treeSet.add(2);
+        treeSet.add(4);
+        System.out.println(treeSet);
+    }
+}
